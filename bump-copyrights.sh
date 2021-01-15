fdbdc3829b1ba551d6956f504bfeb05a3efaa5d3
Przemek Kitszel <53940034+pkitszel@users.noreply.github.com>
2021-01-15 17:22:41 +0100

    Create bump-copyrights.sh

---
+++ bump-copyrights.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -euo pipefail
+
+git-touched-files(){
+	{
+		git diff --name-only --cached
+		git diff --name-only "$branch"...HEAD
+		git diff --name-only
+	} | sort -u
+}
+
+branch=${1:-master}
+cd "$(git rev-parse --show-toplevel)" || exit
+
+git-touched-files |
+	xargs -r gawk -i inplace -v year="$(date +%Y)" '
+		ENDFILE {
+			if (!CopyrightPresent)
+				print FILENAME " has no Copyright Present" > "/dev/stderr"
+			CopyrightPresent = 0
+		}
+		/Copyright/ && $0 ~ year {
+			print; CopyrightPresent = 1; next
+		}
+		/Copyright/ && /[^-][0-9]{4}[^-]/ {
+			print gensub(/[0-9]{4}/, "\\0-" year, 1)
+			TotChanges += CopyrightPresent = 1; next
+		 }
+		/Copyright/ && /[0-9]{4}-[0-9]{4}/ {
+			print gensub(/([0-9]{4}-)[0-9]{4}/, "\\1" year, 1)
+			TotChanges += CopyrightPresent = 1; next
+		}
+		1
+		END {
+			if (TotChanges)
+				print TotChanges, "files updated" > "/dev/stderr"
+		}'
